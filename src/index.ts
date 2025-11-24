11f4bc0628551c7ae507bd984a31b33ca6bbc8fc
IronBlood <aplshenyang@gmail.com>
2025-11-24 22:23:19 +0800

    fix: register build command

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { Cli } from 'clipanion'
 
+import { BuildCommand } from './commands/build.js'
 import { CLI_VERSION } from './utils/misc.js'
 
 export const cli = new Cli({
   binaryName: 'napi-build',
   binaryVersion: CLI_VERSION,
 })
+
+cli.register(BuildCommand)
